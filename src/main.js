2f6b4f99e1b12a71b7a3b325cf16efeefe35d529
LeonidShv <matembit1996@gmail.com>
2023-08-08 11:50:18 +0300

    infra: setup the Apollo-client

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
+const httpLink = createHttpLink({
+    uri: 'https://spacex-production.up.railway.app/',
+})
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+})
+
+const app = createApp({
+    setup() {
+        provide(DefaultApolloClient, apolloClient)
+    },
+
+    render: () => h(App),
+})
+
+app.mount('#app')
